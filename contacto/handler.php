b082059a3908e2beac0ebd83815aef000fea74d7
ivancardozo11 <ivancardozo11@hotmail.com>
2018-10-14 22:04:14 -0300

    bug corregido

--- contacto/handler.php
+++ contacto/handler.php
@@ -21,6 +21,6 @@ $validator->field('message')->maxLength(6000);
 
 
 
-$pp->sendEmailTo('ivancardozo11@hotmail.com'); // ← Your email here
+$pp->sendEmailTo(''); // ← Your email here
 
 echo $pp->process($_POST);
